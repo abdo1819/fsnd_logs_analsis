3e571213e2c5b3c5af66492b9d8a2a8871e1cb65
abdo ragab hashem <ar1813@fayoum.edu.eg>
2019-02-17 05:02:20 +0200

    init

---
+++ readme.md
@@ -0,0 +1,170 @@
+# introduction
+
+this python module for reporting data in news database
+
+avilabe options are to :
+* view top articles
+* view top authors
+* view days with high error rate
+
+
+# using program
+> python3 reporter.py [option [number] ]
+
+### option:
+* atricles
+* author
+* error
+### number:
+* any positive number for atricles,author
+* precent from 0 to 100 for error report
+
+sample usage
+## run three queries
+
+## (1) to view top three atricles 
+> python3.7 reporter.py articles 3
+```
+Candidate is jerk, alleges rival        338647
+Bears love berries, alleges bear        253801
+Bad things gone, say good people        170098
+```
+
+## (2) to view top authors 
+> python3.7 reporter.py author
+```
+Ursula La Multa               507594
+Rudolf von Treppenwitz        423457
+Anonymous Contributor         170098
+Markoff Chaney                 84557
+```
+
+## (3) to view error precentage pathes 1%
+> python3.7 reporter.py error 1
+```
+2016-07-17    2.3175416133162612
+```
+
+## view all qeuries
+> python3.7 reporter.py
+
+```
+
+top articles:
+Candidate is jerk, alleges rival          338647
+Bears love berries, alleges bear          253801
+Bad things gone, say good people          170098
+Goats eat Google's lawn                    84906
+Trouble for troubled troublemakers         84810
+Balloon goons doomed                       84557
+There are a lot of bears                   84504
+Media obsessed with bears                  84383
+
+
+top authors:
+Ursula La Multa               507594
+Rudolf von Treppenwitz        423457
+Anonymous Contributor         170098
+Markoff Chaney                 84557
+
+error report:
+2016-07-17    2.3175416133162612
+```
+
+## view error higher than .75%
+> python3.7 reporter.py error .75
+```
+2016-07-05    0.78463952737007292532
+2016-07-06    0.76126730779839288013
+2016-07-08    0.76009501187648456057
+2016-07-17    2.3175416133162612
+2016-07-19    0.76763560956088332666
+2016-07-21    0.76519896999470387348
+2016-07-24    0.78554163920017578554
+```
+## to view top two authors 
+> python3.7 reporter.py author 2
+```
+Ursula La Multa               507594
+Rudolf von Treppenwitz        423457
+```
+
+# reqierd views
+ you don't need to create those views they are already included in python source code as create or replace
+
+## view for articles and its views count
+```sql
+CREATE OR REPLACE view visits_articles as
+select a.slug , count(log.path)
+from log
+right join articles as a
+on (log.status like '%%200%%')
+and (log.path like concat('%%/article/%%',a.slug))
+group by a.slug 
+```
+## error count for every day
+```sql
+CREATE OR REPLACE view error_count as
+select date(time) as date, count(*) as error
+from log
+where (status like '%%4%%' or status like '%%5%%')
+group by date;
+```
+## success count for every day
+```sql
+CREATE OR REPLACE view success_count as
+select date(time) as date , count(*) as success
+from log
+where (status like '%%2%%')
+group by date;
+```
+
+
+# farther testing to check recived data data 
+## verifay numbers of visits compare to top articles number
+```sql
+select count(*) from log 
+where (log.status like '%%200%%') 
+and (log.path like '%%/article/%%');
+```
+`1185706`
+
+>python3.7 reporter.py articles
+```
+Candidate is jerk, alleges rival          338647
+Bears love berries, alleges bear          253801
+Bad things gone, say good people          170098
+Goats eat Google's lawn                    84906
+Trouble for troubled troublemakers         84810
+Balloon goons doomed                       84557
+There are a lot of bears                   84504
+Media obsessed with bears                  84383
+```
+`338647+253801+170098+84906+84810+84557+84504+84383=1185706`
+
+## finding spam urls
+```sql
+select path from log 
+where not (path like '%/article/%' or path like '/') limit 20;
+```
+```
+         path          
+------------------------
+ /+++ATH0
+ /+++ATH0
+ /+++ATH0
+ /spam-spam-spam-humbug
+ /+++ATH0
+ /+++ATH0
+ /%20%20%20
+ /+++ATH0
+ /%20%20%20
+ /+++ATH0
+ /%20%20%20
+ /%20%20%20
+```
+
+
+
+# style checking
+> pycodestyle reporter.py 
---
+++ reporter.py
@@ -0,0 +1,172 @@
+#!/usr/bin/env python3.7
+import psycopg2
+
+
+# first question
+def top_articals(num=99, view_only=False):
+    """
+    the most popular ? articles sorted by most viewed
+    return list of articles and views for each
+    """
+    # create connection
+    db = psycopg2.connect("dbname=news")
+    cur = db.cursor()
+
+    # creating a view represinting
+    #   articles.slug , view_counts
+    #   whith code 200 statue
+    #   path contain /articales/<article slug>
+    cur.execute('''CREATE OR REPLACE view visits_articles as
+                    select a.slug , count(log.path)
+                    from log
+                    right join articles as a
+                    on (log.status like '%%200%%')
+                    and (log.path like concat('%%/article/%%',a.slug))
+                    group by a.slug ''')
+    # previsouse view will be need to calculate top authors
+    #  so escape after needed
+    if view_only:
+        # commit and closer connection
+        db.commit()
+
+        cur.close()
+        db.close()
+        return
+
+    # preparing reqierd table of
+    #   articles.title   , views_count
+    cur.execute('''SELECT a.title , count
+                    from visits_articles , articles as a
+                    where a.slug = visits_articles.slug
+                    order by count desc
+                    limit %s;''', (num, ))
+
+    res = cur.fetchall()
+
+    # commit and closer connection
+    db.commit()
+
+    cur.close()
+    db.close()
+    return res
+
+
+# second question
+def top_author(num=99):
+    """the most popular ? author sorted by page views"""
+    # create connection
+    db = psycopg2.connect("dbname=news")
+    cur = db.cursor()
+
+    # create view of articals and views for each of them
+    top_articals(view_only=True)
+
+    # creating list of author and  sum of view_count  for thier articles
+    cur.execute('''SELECT authors.name, sum(visits_articles.count)
+                    from authors,articles,visits_articles
+                    where articles.slug = visits_articles.slug
+                    and authors.id = articles.author
+                    group by authors.name
+                    order by sum desc
+                    limit %s''', (num, ))
+
+    res = cur.fetchall()
+
+    # commit and close connection
+    db.commit()
+
+    cur.close()
+    db.close()
+
+    return res
+
+
+# third question
+def error_find(precentage=.01):
+    """ error precentage higher than 1% default"""
+    # create connection
+    db = psycopg2.connect("dbname=news")
+    cur = db.cursor()
+
+    # counting error by day
+    # ignoring redirection statues 3xx
+    # counting server and client error 4xx , 5xx
+
+    # and date(time) = date(%s)
+    cur.execute('''CREATE OR REPLACE view error_count as
+                    select date(time) as date, count(*) as error
+                    from log
+                    where (status like '%%4%%' or status like '%%5%%')
+                    group by date;''')
+
+    # counting sucess by day
+    # count suceess 2xx
+    cur.execute('''CREATE OR REPLACE view success_count as
+                    select date(time) as date , count(*) as success
+                    from log
+                    where (status like '%%2%%')
+                    group by date;''')
+
+    # fing date where error higher than precentage
+    # multiplying first to convert ro float
+    cur.execute('''SELECT er.date , (100.0*er.error)/sucs.success
+                    from error_count as er ,success_count as sucs
+                    where er.date = sucs.date
+                    and cast(er.error as float)/sucs.success >= %s;''',
+                (precentage, ))
+
+    res = cur.fetchall()
+    # commit and close connection
+    db.commit()
+
+    cur.close()
+    db.close()
+
+    return res
+
+
+def view_data(res):
+    '''
+    print two coulomn data in formated way
+    '''
+    if len(res) == 0:
+        print('no data')
+        return
+    width = max([len(str(i[0])) for i in res])+4
+    for i in res:
+        print('{:<{width}}{:>10}'.format(str(i[0]), i[1], width=width))
+
+
+if __name__ == "__main__":
+    import sys
+
+    if len(sys.argv) == 1:
+        print('\ntop articles:')
+        res = top_articals()
+        view_data(res)
+
+        print('\ntop authors:')
+        res = top_author()
+        view_data(res)
+
+        print('\nerror report:')
+        res = error_find()
+        view_data(res)
+
+    elif len(sys.argv) == 2:
+        if (sys.argv[1] == 'articles'):
+            res = top_articals()
+        elif(sys.argv[1] == 'author'):
+            res = top_author()
+        elif(sys.argv[1] == 'error'):
+            res = error_find()
+        view_data(res)
+
+    elif len(sys.argv) == 3:
+        if (sys.argv[1] == 'articles'):
+            res = top_articals(int(sys.argv[2]))
+        elif(sys.argv[1] == 'author'):
+            res = top_author(int(sys.argv[2]))
+        elif(sys.argv[1] == 'error'):
+            res = error_find(float(sys.argv[2])/100.0)
+        view_data(res)
